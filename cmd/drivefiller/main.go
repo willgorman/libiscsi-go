d6ed30e4b23124ce52a87a62b558b4e79dd32211
Will Gorman <will.gorman@joyent.com>
2023-11-27 18:25:22 -0600

    Fill a volume to X% with random data

--- cmd/drivefiller/main.go
+++ cmd/drivefiller/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
 	"log"
 	"os"
+	"strconv"
 
-	"github.com/sanity-io/litter"
 	iscsi "github.com/willgorman/libiscsi-go"
 )
 
 func main() {
-	if len(os.Args) != 3 {
+	if len(os.Args) != 4 {
 		panic("missing required args")
 	}
 	device := iscsi.New(iscsi.ConnectionDetails{
@@ -17,10 +18,20 @@ func main() {
 		TargetURL:    os.Args[2],
 	})
 
-	err := device.Connect()
+	percentage, err := strconv.Atoi(os.Args[3])
+	if err != nil {
+		panic(err)
+	}
+
+	if percentage < 1 || percentage > 100 {
+		panic("percentage must be between 1 and 100")
+	}
+
+	err = device.Connect()
 	if err != nil {
 		log.Fatalln(err)
 	}
+	log.Println("connected")
 
 	defer func() {
 		_ = device.Disconnect()
@@ -31,32 +42,29 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	data := []byte("hello iscsi")
-	// TODO: (willgorman) handle data > block size or just let it truncate?
-	if len(data) < capacity.BlockSize {
-		dataCopy := make([]byte, capacity.BlockSize)
-		copy(dataCopy, data)
-		data = dataCopy
+	// TODO: (willgorman) what's max throughput here?  can we split up and run chunks in parallel?
+	log.Println(percentage)
+	log.Println(float64(percentage) / 100.0)
+	blocksToWrite := int(float64(float64(capacity.LBA)) * float64(percentage) / 100.0)
+	log.Println(blocksToWrite)
+	currentBlock := 0
+	for currentBlock < blocksToWrite {
+		// fixme: handle case where blocksToWrite - currentBlock < 1024
+		data := make([]byte, 1024*capacity.BlockSize)
+		_, err := rand.Read(data)
+		if err != nil {
+			panic(err)
+		}
+		err = device.Write16(iscsi.Write16{
+			LBA:       currentBlock,
+			Data:      data,
+			BlockSize: capacity.BlockSize,
+		})
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Printf("wrote 1024 blocks starting at %d", currentBlock)
+		// write 1024 blocks at once?
+		currentBlock = currentBlock + 1024
 	}
-
-	litter.Dump(string(data))
-
-	err = device.Write16(iscsi.Write16{
-		LBA:       0,
-		Data:      data,
-		BlockSize: capacity.BlockSize,
-	})
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	dataread, err := device.Read16(iscsi.Read16{
-		LBA:       0,
-		Blocks:    1,
-		BlockSize: 512,
-	})
-	if err != nil {
-		log.Fatalln(err)
-	}
-	litter.Dump("hey!", string(dataread))
 }
