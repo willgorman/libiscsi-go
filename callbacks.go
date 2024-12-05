1f1297cb46979e00caeaed1dadc1b0ca48b6f173
Will Gorman <will.gorman@joyent.com>
2024-12-05 16:21:44 -0600

    Fixes #2
    The sync libiscsi apis return an error if poll is interrupted by a signal and there's no way to recover without retrying the iscsi command.  Instead
    we can use the async api and implement our own poll loop (with retry on EINTR) to wait for the results

--- callbacks.go
+++ callbacks.go
@@ -8,11 +8,21 @@ package iscsi
 extern void iscsiChannelCB(struct iscsi_context*, int,
 				 void*, void*);
 
+extern void iscsiSyncCB(struct iscsi_context*, int,
+				 void*, void*);
+
 void iscsiChannelCB_cgo(struct iscsi_context *iscsi, int status,
 				 void *command_data, void *private_data) {
   iscsiChannelCB(iscsi, status, command_data, private_data);
+}
+
+void iscsiSyncCB_cgo(struct iscsi_context *iscsi, int status,
+				 void *command_data, void *private_data) {
+  iscsiSyncCB(iscsi, status, command_data, private_data);
 }
 */
 import "C"
 
 var channelCB = C.iscsi_command_cb(C.iscsiChannelCB_cgo)
+
+var syncCB = C.iscsi_command_cb(C.iscsiSyncCB_cgo)
